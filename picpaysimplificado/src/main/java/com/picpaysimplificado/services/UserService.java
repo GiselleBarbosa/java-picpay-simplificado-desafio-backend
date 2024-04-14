8013dd079b26fe7e5e8ec7d36610cc0d6dcc768b
GiselleBarbosa <g88barbosa@gmail.com>
2024-04-14 18:12:38 -0300

    feat: adicionado userService

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,35 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+            if(sender.getUserType() == UserType.MERCHANT){
+                throw new Exception("Usuário do tipo Lojista não está autorizado a realizar esta transação.");
+        }
+
+            if(sender.getBalance().compareTo(amount) <= 0){
+                throw new Exception("Saldo insulficiente para realizar esta transação.");
+            }
+    }
+
+    public User findUserById(Long id ) throws Exception {
+       return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado."));
+    }
+
+    public void saveUser(User user) {
+        this.userRepository.save(user);
+    }
+
+}
