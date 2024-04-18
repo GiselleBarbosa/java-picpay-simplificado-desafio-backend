b3202e3ebee3e489b048f7a12d396483b2bc3be3
GiselleBarbosa <g88barbosa@gmail.com>
2024-04-18 09:29:43 -0300

    feat: adicionado UserController

--- picpaysimplificado/src/main/java/com/picpaysimplificado/controllers/UserController.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/controllers/UserController.java
@@ -4,8 +4,8 @@ import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +16,8 @@ public class UserController {
     private UserService userService;
     @PostMapping
     public ResponseEntity<User>createUser(UserDTO userDTO){
-        User newUser = userService.createUser(userDTO)
-
+        User newUser = userService.createUser(userDTO);
+        return new ResponseEntity<User>(newUser,HttpStatus.CREATED);
     }
 
 }
