c2d4ea4c814923facd7542c6ea9dc2d6459e34af
GiselleBarbosa <g88barbosa@gmail.com>
2024-04-18 09:11:59 -0300

    feat: add uma nova instancia para RestTemplate e appConfig

--- picpaysimplificado/src/main/java/com/picpaysimplificado/services/NotificationService.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -5,8 +5,10 @@ import com.picpaysimplificado.dtos.NotificationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class NotificationService {
     @Autowired
     private RestTemplate restTemplate;
@@ -25,7 +27,6 @@ public class NotificationService {
        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
            System.out.println("Erro ao enviar a notificacao");
            throw new Exception("Serviço de notificação está fora do ar.");
-
        }
     }
 }
