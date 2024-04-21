0f59f5d970257fa4ca1bc50d791757eea2f58df4
GiselleBarbosa <g88barbosa@gmail.com>
2024-04-21 12:05:58 -0300

    feat: adicionado getAllUser e anotacao RequestMapping faltante

--- picpaysimplificado/src/main/java/com/picpaysimplificado/controllers/UserController.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/controllers/UserController.java
@@ -6,18 +6,27 @@ import com.picpaysimplificado.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/users")
+import java.util.List;
+
+@RestController()
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
     @PostMapping
-    public ResponseEntity<User>createUser(UserDTO userDTO){
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
         User newUser = userService.createUser(userDTO);
-        return new ResponseEntity<User>(newUser,HttpStatus.CREATED);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
 }
