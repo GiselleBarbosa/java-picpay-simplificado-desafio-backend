ffb0b8e14823a38a96ab021823dfef815533c774
GiselleBarbosa <g88barbosa@gmail.com>
2024-04-21 12:06:48 -0300

    feat: adicionado createUser e getAllUsers no servico

--- picpaysimplificado/src/main/java/com/picpaysimplificado/services/UserService.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/services/UserService.java
@@ -2,11 +2,13 @@ package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -15,21 +17,32 @@ public class UserService {
     private UserRepository userRepository;
 
     public void validateTransaction(User sender, BigDecimal amount) throws Exception {
-            if(sender.getUserType() == UserType.MERCHANT){
-                throw new Exception("Usuário do tipo Lojista não está autorizado a realizar esta transação.");
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo Lojista não está autorizado a realizar esta transação.");
         }
 
-            if(sender.getBalance().compareTo(amount) <= 0){
-                throw new Exception("Saldo insulficiente para realizar esta transação.");
-            }
+        if (sender.getBalance().compareTo(amount) <= 0) {
+            throw new Exception("Saldo insulficiente para realizar esta transação.");
+        }
     }
 
-    public User findUserById(Long id ) throws Exception {
-       return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado."));
+    public User findUserById(Long id) throws Exception {
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado."));
+    }
+
+    public User createUser(UserDTO userData) {
+        User newUser = new User(userData);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
     }
 
     public void saveUser(User user) {
         this.userRepository.save(user);
     }
+
 
 }
